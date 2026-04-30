8329dfcad22208d75ed97b4bfec446b031d82579
ZarateMIG <zaratemiguel.jj@gmail.com>
2026-04-30 10:36:47 -0300

    Gestión de piezas: caída, giro y traslación.
    Modificación en la lógica de caída y giro de las piezas, creación de la lógica de mover las piezas de izquierda a derecha

--- tetris/tetris/main.c
+++ tetris/tetris/main.c
@@ -3,96 +3,175 @@
 #include <time.h>
 #include "GBT/gbt.h"
 
-#define TAM_VENTANA_X 640
-#define TAM_VENTANA_Y 480
+#define TAM_VENTANA_X  640
+#define TAM_VENTANA_Y  480
 
 #define FILAS 22
 #define COLS 10
-#define TAM_MINO 20  // Tamaño en píxeles de cada cuadradito
+#define TAM_MINO 12
 
-#define PIEZAS 7 // cantidad de piezas diferentes
-#define PRUEBA 2 // que pieza quiere que aparesca 0-I,1-L,2-J,3-S,4-Z,5-O,6-T
-
+#define PIEZAS 7
+#define PRUEBA 0
 
 void dibujar_tablero(uint8_t mat[FILAS][COLS]);
-void figueras_tetris(uint8_t mat[FILAS][COLS],int mat_coor [PIEZAS][8],int *j,int *nivel); //*j y *nivel no hacen nada momentaneamente
-void figuras_caen (uint8_t mat[FILAS][COLS],int mat_coor [PIEZAS][8],int *j,int *nivel);
-void borrar (uint8_t mat[FILAS][COLS],int mat_coor [PIEZAS][8],int *j,int *nivel);
+void figueras_tetris(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+void figuras_caen(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+void borrar(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+
+void rotar_universal(int pieza[8], uint8_t tablero[FILAS][COLS]);
+void mover_derecha(int pieza[8], uint8_t tablero[FILAS][COLS]);
+void mover_izquierda(int pieza[8], uint8_t tablero[FILAS][COLS]);
+
+int verificar_colision(uint8_t mat[FILAS][COLS], int pieza[8]);
+void fijar_pieza(uint8_t mat[FILAS][COLS], int pieza[8]);
+void reiniciar_pieza(int pieza[8]);
 
 int main()
 {
     if (gbt_iniciar() < 0)
-    {
-        printf("ERROR AL INICIAR LA BIBLIOTECA GBT %s", gbt_obtener_log());
         return 1;
-    }
 
-    if(gbt_crear_ventana("Tetris UNLaM - Luminosos", TAM_VENTANA_X, TAM_VENTANA_Y, 2) < 0)
-    {
-        printf("ERROR AL CREAR LA VENTANA %s", gbt_obtener_log());
+    if (gbt_crear_ventana("Tetris UNLaM - Luminosos", TAM_VENTANA_X, TAM_VENTANA_Y, 2) < 0)
         return 1;
-    }
 
     srand(time(NULL));
 
     uint8_t tablero[FILAS][COLS] = {0};
     int juego_corriendo = 1;
     int j = 0;
-    int nivel = 1;
+    int nivel = 0;
 
-    int mat_coordenads [PIEZAS][8] = {{0,3,0,4,0,5,0,6},
-                                      {1,4,1,5,1,6,0,6},
-                                      {1,4,1,5,1,6,0,4},
-                                      {1,4,1,5,0,5,0,6},
-                                      {1,5,1,6,0,4,0,5},
-                                      {0,4,0,5,1,4,1,5},
-                                      {1,3,1,4,1,5,0,4}};
+    int mat_coordenads[PIEZAS][8] = {{0,3,0,4,0,5,0,6},
+                                     {1,4,1,5,1,6,0,6},
+                                     {1,4,1,5,1,6,0,4},
+                                     {1,4,1,5,0,5,0,6},
+                                     {1,5,1,6,0,4,0,5},
+                                     {0,4,0,5,1,4,1,5},
+                                     {1,3,1,4,1,5,0,4}};
+
 
     tGBT_Temporizador* reloj_caida = gbt_temporizador_crear(0.8);
 
-    if (reloj_caida == NULL)
-    {
-        printf("ERROR: No se pudo crear el temporizador.\n");
-        return 1;
-    }
-
-    dibujar_tablero(tablero);
-    gbt_volcar_backbuffer();
-
-    while(juego_corriendo)
+    while (juego_corriendo)
     {
         gbt_procesar_entrada();
 
         if (gbt_tecla_presionada(GBTK_ESCAPE)) juego_corriendo = 0;
 
-        // --- LÓGICA DE TIEMPO ---
+
         if (gbt_temporizador_consumir(reloj_caida))
         {
-            // 1. Borramos la posición vieja
-            borrar(tablero, mat_coordenads, &j, &nivel);
+            if (verificar_colision(tablero, mat_coordenads[PRUEBA]))
+            {
+                fijar_pieza(tablero, mat_coordenads[PRUEBA]);
 
-            // 2. Dibujamos en la posición nueva (que ya tiene la fila + 1)
-            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+                reiniciar_pieza(mat_coordenads[PRUEBA]);
+            }
+            else
+            {
+                borrar(tablero, mat_coordenads, &j, &nivel);
+
+                figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+            }
         }
 
-        // --- RENDERIZADO ---
+        if (gbt_tecla_presionada(GBTK_w) && PRUEBA != 5)
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            rotar_universal(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(150);
+        }
+
+        if (gbt_tecla_presionada(GBTK_d))
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            mover_derecha(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(100);
+        }
+
+        if (gbt_tecla_presionada(GBTK_a))
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            mover_izquierda(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(100);
+        }
+
         gbt_borrar_backbuffer(0);
         dibujar_tablero(tablero);
         gbt_volcar_backbuffer();
-
         gbt_esperar(16);
     }
 
     gbt_temporizador_destruir(reloj_caida);
     gbt_cerrar();
-
     return 0;
+}
+
+void borrar(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    for ((*j) = 0; (*j) < 7; (*j) += 2)
+    {
+        int f = mat_coor[PRUEBA][*j];
+        int c = mat_coor[PRUEBA][*j + 1];
+
+        if (f >= 0 && f < FILAS && mat[f][c] == 1)
+        {
+            mat[f][c] = 0;
+        }
+    }
+
+    for ((*j) = 0; (*j) < 7; (*j) += 2)
+    {
+        mat_coor[PRUEBA][*j] = mat_coor[PRUEBA][*j] + 1;
+    }
+}
+
+void figuras_caen(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    for ((*j) = 0; (*j) < 7; (*j) = (*j) + 2)
+    {
+        int f = mat_coor[PRUEBA][*j];
+
+        int c = mat_coor[PRUEBA][*j + 1];
+
+        if (f >= 0 && f < FILAS)
+        {
+            mat[f][c] = 1;
+        }
+    }
+}
+
+void figueras_tetris(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    figuras_caen(mat, mat_coor, j, nivel);
 }
 
 void dibujar_tablero(uint8_t mat[FILAS][COLS])
 {
     int pos_pantalla_x = (TAM_VENTANA_X / 2) - (COLS * TAM_MINO)/2;
-    int pos_pantalla_y = 40;
+
+    int pos_pantalla_y = 70;
 
     for (int f = 0; f < FILAS; f++)
     {
@@ -100,7 +179,6 @@ void dibujar_tablero(uint8_t mat[FILAS][COLS])
         {
             int px = pos_pantalla_x + (c * TAM_MINO);
             int py = pos_pantalla_y + (f * TAM_MINO);
-
 
             int color_borde = (mat[f][c] == 0) ? 8 : 7;
 
@@ -118,35 +196,107 @@ void dibujar_tablero(uint8_t mat[FILAS][COLS])
 }
 
 
-
-void figueras_tetris(uint8_t mat[FILAS][COLS],int mat_coor [PIEZAS][8],int *j,int *nivel)
+int verificar_colision(uint8_t mat[FILAS][COLS], int pieza[8])
 {
-    //int pieza = ; //(rand() % 7) + 1;
+    int prox_f;
+    int c;
 
-    figuras_caen (mat,mat_coor,j,nivel);
+    for (int i = 0; i < 8; i += 2)
+    {
+        prox_f = pieza[i] + 1;
+        c = pieza[i+1];
+
+
+        if (prox_f >= FILAS || (prox_f >= 0 && mat[prox_f][c] == 2))
+            return 1;
+    }
+
+    return 0;
 }
 
-void figuras_caen (uint8_t mat[FILAS][COLS],int mat_coor [PIEZAS][8],int *j,int *nivel)
+void fijar_pieza(uint8_t mat[FILAS][COLS], int pieza[8])
 {
-
-    for((*j) = 0; (*j) < 7; (*j) = (*j) + 2)
+    int f;
+    int c;
+    for (int i = 0; i < 8; i += 2)
     {
-            mat[mat_coor [PRUEBA][*j]] [ mat_coor [PRUEBA][(*j)+1] ] = 1;
-            dibujar_tablero(mat);
+        f = pieza[i];
+        c = pieza[i+1];
 
-            gbt_volcar_backbuffer();
-
-
+        if (f >= 0 && f < FILAS)
+            mat[f][c] = 2;
     }
 }
 
-void borrar (uint8_t mat[FILAS][COLS], int mat_coor [PIEZAS][8], int *j, int *nivel)
+void reiniciar_pieza(int pieza[8])
 {
-    for((*j) = 0; (*j) < 7; (*j) += 2)
-        mat[mat_coor[PRUEBA][*j]][mat_coor[PRUEBA][*j + 1]] = 0;
+    int iniciales[8] = {0,3,0,4,0,5,0,6};
+
+    for(int i=0; i<8; i++)
+        pieza[i] = iniciales[i];
+}
 
 
-    for((*j) = 0; (*j) < 7; (*j) += 2)
-        mat_coor[PRUEBA][*j] = mat_coor[PRUEBA][*j] + 1;
+void rotar_universal(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int f_pivote = pieza[2], c_pivote = pieza[3], aux[8], posible = 1;
+    int f_rel;
+    int c_rel;
 
+    for (int i = 0; i < 8; i += 2)
+    {
+        f_rel = pieza[i] - f_pivote, c_rel = pieza[i+1] - c_pivote;
+
+        aux[i] = f_pivote + c_rel; aux[i+1] = c_pivote - f_rel;
+
+        if (aux[i] < 0 || aux[i] >= FILAS || aux[i+1] < 0 || aux[i+1] >= COLS || tablero[aux[i]][aux[i+1]] == 2)
+        {
+            posible = 0;
+            break;///REVISAR LOGICA
+        }
+    }
+    if (posible)
+        for (int i = 0; i < 8; i++)
+            pieza[i] = aux[i];
+}
+
+void mover_derecha(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int posible = 1;
+    int c_nueva;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        c_nueva = pieza[i+1] + 1;
+
+        if (c_nueva >= COLS || tablero[pieza[i]][c_nueva] == 2)
+            posible = 0;
+             break; ///REVISAR LOGICA
+
+
+    }
+
+    if (posible)
+        for (int i = 1; i < 8; i += 2)
+            pieza[i]++;
+}
+
+void mover_izquierda(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int posible = 1;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        int c_nueva = pieza[i+1] - 1;
+
+        if (c_nueva < 0 || tablero[pieza[i]][c_nueva] == 2)
+        {
+            posible = 0;
+            break;///REVISAR LOGICA
+        }
+    }
+
+    if (posible)
+        for (int i = 1; i < 8; i += 2)
+            pieza[i]--;
 }
---
+++ tetris/tetris/tetris/main.c
@@ -0,0 +1,301 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include "GBT/gbt.h"
+
+#define TAM_VENTANA_X  640
+#define TAM_VENTANA_Y  480
+
+#define FILAS 22
+#define COLS 10
+#define TAM_MINO 12
+
+#define PIEZAS 7
+#define PRUEBA 0
+
+void dibujar_tablero(uint8_t mat[FILAS][COLS]);
+void figueras_tetris(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+void figuras_caen(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+void borrar(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel);
+
+void rotar_universal(int pieza[8], uint8_t tablero[FILAS][COLS]);
+void mover_derecha(int pieza[8], uint8_t tablero[FILAS][COLS]);
+void mover_izquierda(int pieza[8], uint8_t tablero[FILAS][COLS]);
+
+int verificar_colision(uint8_t mat[FILAS][COLS], int pieza[8]);
+void fijar_pieza(uint8_t mat[FILAS][COLS], int pieza[8]);
+void reiniciar_pieza(int pieza[8]);
+
+int main()
+{
+    if (gbt_iniciar() < 0)
+        return 1;
+
+    if (gbt_crear_ventana("Tetris UNLaM - Luminosos", TAM_VENTANA_X, TAM_VENTANA_Y, 2) < 0)
+        return 1;
+
+    srand(time(NULL));
+
+    uint8_t tablero[FILAS][COLS] = {0};
+    int juego_corriendo = 1;
+    int j = 0;
+    int nivel = 0;
+
+    int mat_coordenads[PIEZAS][8] = {{0,3,0,4,0,5,0,6},
+                                     {1,4,1,5,1,6,0,6},
+                                     {1,4,1,5,1,6,0,4},
+                                     {1,4,1,5,0,5,0,6},
+                                     {1,5,1,6,0,4,0,5},
+                                     {0,4,0,5,1,4,1,5},
+                                     {1,3,1,4,1,5,0,4}};
+
+
+    tGBT_Temporizador* reloj_caida = gbt_temporizador_crear(0.8);
+
+    while (juego_corriendo)
+    {
+        gbt_procesar_entrada();
+
+        if (gbt_tecla_presionada(GBTK_ESCAPE)) juego_corriendo = 0;
+
+
+        if (gbt_temporizador_consumir(reloj_caida))
+        {
+            if (verificar_colision(tablero, mat_coordenads[PRUEBA]))
+            {
+                fijar_pieza(tablero, mat_coordenads[PRUEBA]);
+
+                reiniciar_pieza(mat_coordenads[PRUEBA]);
+            }
+            else
+            {
+                borrar(tablero, mat_coordenads, &j, &nivel);
+
+                figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+            }
+        }
+
+        if (gbt_tecla_presionada(GBTK_w) && PRUEBA != 5)
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            rotar_universal(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(150);
+        }
+
+        if (gbt_tecla_presionada(GBTK_d))
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            mover_derecha(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(100);
+        }
+
+        if (gbt_tecla_presionada(GBTK_a))
+        {
+            borrar(tablero, mat_coordenads, &j, &nivel);
+
+            for(int i=0; i<8; i+=2)
+                mat_coordenads[PRUEBA][i]--;
+
+            mover_izquierda(mat_coordenads[PRUEBA], tablero);
+
+            figueras_tetris(tablero, mat_coordenads, &j, &nivel);
+
+            gbt_esperar(100);
+        }
+
+        gbt_borrar_backbuffer(0);
+        dibujar_tablero(tablero);
+        gbt_volcar_backbuffer();
+        gbt_esperar(16);
+    }
+
+    gbt_temporizador_destruir(reloj_caida);
+    gbt_cerrar();
+    return 0;
+}
+
+void borrar(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    for ((*j) = 0; (*j) < 7; (*j) += 2)
+    {
+        int f = mat_coor[PRUEBA][*j];
+        int c = mat_coor[PRUEBA][*j + 1];
+
+        if (f >= 0 && f < FILAS && mat[f][c] == 1)
+        {
+            mat[f][c] = 0;
+        }
+    }
+
+    for ((*j) = 0; (*j) < 7; (*j) += 2)
+    {
+        mat_coor[PRUEBA][*j] = mat_coor[PRUEBA][*j] + 1;
+    }
+}
+
+void figuras_caen(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    for ((*j) = 0; (*j) < 7; (*j) = (*j) + 2)
+    {
+        int f = mat_coor[PRUEBA][*j];
+
+        int c = mat_coor[PRUEBA][*j + 1];
+
+        if (f >= 0 && f < FILAS)
+        {
+            mat[f][c] = 1;
+        }
+    }
+}
+
+void figueras_tetris(uint8_t mat[FILAS][COLS], int mat_coor[PIEZAS][8], int *j, int *nivel)
+{
+    figuras_caen(mat, mat_coor, j, nivel);
+}
+
+void dibujar_tablero(uint8_t mat[FILAS][COLS])
+{
+    int pos_pantalla_x = (TAM_VENTANA_X / 2) - (COLS * TAM_MINO)/2;
+
+    int pos_pantalla_y = 70;
+
+    for (int f = 0; f < FILAS; f++)
+    {
+        for (int c = 0; c < COLS; c++)
+        {
+            int px = pos_pantalla_x + (c * TAM_MINO);
+            int py = pos_pantalla_y + (f * TAM_MINO);
+
+            int color_borde = (mat[f][c] == 0) ? 8 : 7;
+
+            for (int i = 0; i < TAM_MINO; i++)
+                for (int j = 0; j < TAM_MINO; j++)
+                    gbt_dibujar_pixel(px + i, py + j, color_borde);
+
+            int color_centro = (mat[f][c] == 0) ? 0 : mat[f][c];
+
+            for (int i = 1; i < TAM_MINO - 1; i++)
+                for (int j = 1; j < TAM_MINO - 1; j++)
+                    gbt_dibujar_pixel(px + i, py + j, color_centro);
+        }
+    }
+}
+
+
+int verificar_colision(uint8_t mat[FILAS][COLS], int pieza[8])
+{
+    int prox_f;
+    int c;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        prox_f = pieza[i] + 1;
+        c = pieza[i+1];
+
+
+        if (prox_f >= FILAS || (prox_f >= 0 && mat[prox_f][c] == 2))
+            return 1;
+    }
+
+    return 0;
+}
+
+void fijar_pieza(uint8_t mat[FILAS][COLS], int pieza[8])
+{
+    int f;
+    int c;
+    for (int i = 0; i < 8; i += 2)
+    {
+        f = pieza[i];
+        c = pieza[i+1];
+
+        if (f >= 0 && f < FILAS)
+            mat[f][c] = 2;
+    }
+}
+
+void reiniciar_pieza(int pieza[8])
+{
+    int iniciales[8] = {0,3,0,4,0,5,0,6};
+
+    for(int i=0; i<8; i++)
+        pieza[i] = iniciales[i];
+}
+
+
+void rotar_universal(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int f_pivote = pieza[2], c_pivote = pieza[3], aux[8], posible = 1;
+    int f_rel;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        f_rel = pieza[i] - f_pivote, c_rel = pieza[i+1] - c_pivote;
+
+        aux[i] = f_pivote + c_rel; aux[i+1] = c_pivote - f_rel;
+
+        if (aux[i] < 0 || aux[i] >= FILAS || aux[i+1] < 0 || aux[i+1] >= COLS || tablero[aux[i]][aux[i+1]] == 2)
+        {
+            posible = 0;
+            break;///REVISAR LOGICA
+        }
+    }
+    if (posible)
+        for (int i = 0; i < 8; i++)
+            pieza[i] = aux[i];
+}
+
+void mover_derecha(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int posible = 1;
+    int c_nueva;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        c_nueva = pieza[i+1] + 1;
+
+        if (c_nueva >= COLS || tablero[pieza[i]][c_nueva] == 2)
+            posible = 0;
+             break; ///REVISAR LOGICA
+
+
+    }
+
+    if (posible)
+        for (int i = 1; i < 8; i += 2)
+            pieza[i]++;
+}
+
+void mover_izquierda(int pieza[8], uint8_t tablero[FILAS][COLS])
+{
+    int posible = 1;
+
+    for (int i = 0; i < 8; i += 2)
+    {
+        int c_nueva = pieza[i+1] - 1;
+
+        if (c_nueva < 0 || tablero[pieza[i]][c_nueva] == 2)
+        {
+            posible = 0;
+            break;///REVISAR LOGICA
+        }
+    }
+
+    if (posible)
+        for (int i = 1; i < 8; i += 2)
+            pieza[i]--;
+}
