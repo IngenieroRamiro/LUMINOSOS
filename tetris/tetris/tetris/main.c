29bcd0942af5ac438fc83864e16c6db6b9d58610
IngenieroRamiro <rrodriguez230@alumno.unlam.edu.ar>
2026-05-07 21:01:02 -0300

    eliminar lineas
    Agregar eliminar_lineas para limpiar filas completas

--- tetris/tetris/tetris/main.c
+++ tetris/tetris/tetris/main.c
@@ -26,6 +26,8 @@ int verificar_colision(uint8_t mat[FILAS][COLS], int pieza[8]);
 void fijar_pieza(uint8_t mat[FILAS][COLS], int pieza[8]);
 void reiniciar_pieza(int pieza[8]);
 
+int eliminar_lineas(uint8_t mat[FILAS][COLS]);
+
 int main()
 {
     if (gbt_iniciar() < 0)
@@ -64,6 +66,9 @@ int main()
             if (verificar_colision(tablero, mat_coordenads[PRUEBA]))
             {
                 fijar_pieza(tablero, mat_coordenads[PRUEBA]);
+
+                int lineas = eliminar_lineas(tablero);
+                (void)lineas;    // para el puntaje, pendiente
 
                 reiniciar_pieza(mat_coordenads[PRUEBA]);
             }
@@ -240,7 +245,7 @@ void reiniciar_pieza(int pieza[8])
 void rotar_universal(int pieza[8], uint8_t tablero[FILAS][COLS])
 {
     int f_pivote = pieza[2], c_pivote = pieza[3], aux[8], posible = 1;
-    int f_rel;
+    int f_rel, c_rel;
 
     for (int i = 0; i < 8; i += 2)
     {
@@ -298,4 +303,57 @@ void mover_izquierda(int pieza[8], uint8_t tablero[FILAS][COLS])
     if (posible)
         for (int i = 1; i < 8; i += 2)
             pieza[i]--;
+}
+
+int eliminar_lineas(uint8_t mat[FILAS][COLS])
+{
+    int eliminadas = 0;
+    int f = FILAS - 1;
+
+    // voy de abajo para arriba, asi cuando elimino una fila y todo cae, vuelvo a fijarme la misma posicion
+    while (f >= 0)
+    {
+        // me fijo  si la fila f esta toda en 2
+        int completa = 1;
+        uint8_t *p   = mat[f];
+        uint8_t *fin = mat[f] + COLS;
+
+        while (p < fin)
+        {
+            if (*p != 2)
+            {
+                completa = 0;
+                break;
+            }
+            p++;
+        }
+
+        if (completa)
+        {
+            // corro cada fila de arriba un escalon hacia abajo
+            for (int k = f; k > 0; k--)
+            {
+                uint8_t *dst = mat[k];
+                uint8_t *src = mat[k-1];
+                uint8_t *tope = src + COLS;
+
+                while (src < tope)
+                    *dst++ = *src++;
+            }
+
+            // la fila 0 queda vacia
+            uint8_t *q   = mat[0];
+            uint8_t *qfin = mat[0] + COLS;
+            while (q < qfin)
+                *q++ = 0;
+
+            eliminadas++;
+        }
+        else
+        {
+            f--;
+        }
+    }
+
+    return eliminadas;
 }
