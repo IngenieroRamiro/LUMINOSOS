d65357a096523a9ede5867edf88f91db2f2e4185
IngenieroRamiro <rrodriguez230@alumno.unlam.edu.ar>
2026-05-27 14:29:16 -0300

    Revert "arreglo de estadisticas + movimiento izquieda con Q"
    This reverts commit a61a6fd787a32609effc50eeaf6233fafb1771be.

--- tetris/piezas.h
+++ tetris/piezas.h
@@ -1,54 +1,6 @@
 #ifndef PIEZAS_H
 #define PIEZAS_H
 
-<<<<<<< Updated upstream
-=======
-#include "tablero.h"
-#include "tipos.h"
-
-typedef struct
-{
-    int f;
-    int c;
-} tBloque;
-
-typedef struct
-{
-    tBloque bloques[MAX_BLOQUES];
-    int cant_bloques;
-    int tipo;
-    int color;
-} tPieza;
-
-void pieza_generar(tPieza* p, int tipo);
-int pieza_puede_mover(const tTablero* t, const tPieza* p, int moverF, int moverC);
-void pieza_mover(tPieza* p, int moverF, int moverC);
-int pieza_puede_rotar(const tTablero* t, const tPieza* p);
-void pieza_rotar(tPieza* p);
-
-int  pieza_puede_rotar_anti(const tTablero* t, const tPieza* p);
-void pieza_rotar_anti(tPieza* p);
-void pieza_rotar_anti_deluxe(const tTablero* t, tPieza* p);
-
-void pieza_fijar(tTablero* t, const tPieza* p);
-int pieza_color(int figura);
-void pieza_rotar_deluxe(const tTablero* t, tPieza* p);
-void pieza_mover_deluxe_horizontal(const tTablero* t, tPieza* p, int moverC);
-
-
-#endif
-
-
-/// ////////////////////// ///
-/// ARQUITECTURA ANTERIOR ///
-
-/*
-
-
-#ifndef PIEZAS_H
-#define PIEZAS_H
-
->>>>>>> Stashed changes
 #include <stdint.h>
 #include "tipos.h"
 
